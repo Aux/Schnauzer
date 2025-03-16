9c6c814c1763fef3cfd5446117d523a7e418f343
Aux <817531+Aux@users.noreply.github.com>
2025-03-15 23:58:26 -0500

    Remove docker actions

---
+++ src/Schnauzer/Interactions/VoiceOwnerModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schnauzer.Interactions;
+
+public class VoiceOwnerModule : InteractionModuleBase<SocketInteractionContext>
+{
+
+}
