78a0ea54ea8650d930ff72bd707f6db8756bc3d7
Aux <817531+Aux@users.noreply.github.com>
2025-03-16 23:00:24 -0500

    Begin adding voice owner commands

--- src/Schnauzer/Interactions/VoiceOwnerModule.cs
+++ src/Schnauzer/Interactions/VoiceOwnerModule.cs
@@ -1,13 +1,40 @@
-﻿using Discord.Interactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discord;
+using Discord.Interactions;
 
 namespace Schnauzer.Interactions;
 
+// We'll place these commands as mentions inside of the voice owner panel
+
+[Group("voice", "Management commands for channel owners.")]
 public class VoiceOwnerModule : InteractionModuleBase<SocketInteractionContext>
 {
+    [SlashCommand("rename", "Rename a voice channel you own.")]
+    public async Task RenameAsync(string name)
+    {
+        await Task.Delay(0);
+    }
 
+    [SlashCommand("limit", "Change the user limit of a voice channel you own.")]
+    public async Task RenameAsync(int limit)
+    {
+        await Task.Delay(0);
+    }
+
+    [SlashCommand("kick", "Kick a user from a voice channel you own.")]
+    public async Task KickAsync(IGuildUser user, string reason)
+    {
+        await Task.Delay(0);
+    }
+
+    [SlashCommand("block", "Block a user from accessing a voice channel you own.")]
+    public async Task BlockAsync(IGuildUser user, string reason)
+    {
+        await Task.Delay(0);
+    }
+
+    [SlashCommand("give", "Give ownership of a voice channel to another user.")]
+    public async Task GiveAsync(IGuildUser user)
+    {
+        await Task.Delay(0);
+    }
 }
