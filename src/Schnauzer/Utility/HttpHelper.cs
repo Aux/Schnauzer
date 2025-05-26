741f05e5ac3bbffbbf631e4c2efaa6ff90c42096
Aux <817531+Aux@users.noreply.github.com>
2025-05-26 14:24:48 -0500

    Create HttpHelper.cs

---
+++ src/Schnauzer/Utility/HttpHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Schnauzer;
+public static class HttpHelper
+{
+    private static readonly HttpClient _http = new();
+
+    public static Task<string> DownloadStringAsync(string requestUri)
+    {
+        return _http.GetStringAsync(requestUri);
+    }
+}
