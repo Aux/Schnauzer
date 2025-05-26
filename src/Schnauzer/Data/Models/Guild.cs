d5162ddf42ef14c66a45e9c4eb9b53a92c4e1e2d
Aux <817531+Aux@users.noreply.github.com>
2025-05-26 14:24:37 -0500

    Update Guild.cs

--- src/Schnauzer/Data/Models/Guild.cs
+++ src/Schnauzer/Data/Models/Guild.cs
@@ -1,19 +1,29 @@
-﻿namespace Schnauzer.Data.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Schnauzer.Data.Models;
 
 public class Guild
 {
+    [JsonIgnore]
     public required ulong Id { get; set; }
+    [JsonIgnore]
     public bool? IsBanned { get; set; }
-    public ulong? CreateChannelId { get; set; }
+    [JsonIgnore]
     public string PreferredLocale { get; set; }
+    public ulong? CreateChannelId { get; set; }
 
     public List<ulong> CanOwnRoleIds { get; set; }
+    public bool? DenyDeafenedOwnership { get; set; }
+    public bool? DenyMutedOwnership { get; set; }
+
     public int? DefaultLobbySize { get; set; }
     public int? MaxLobbySize { get; set; }
+    public int? MaxLobbyCount { get; set; }
 
     public bool? IsAutoModEnabled { get; set; }
     public ulong? AutoModLogChannelId { get; set; }
     public List<ulong> AutomodRuleIds { get; set; }
 
+    [JsonIgnore]
     public List<Channel> DynamicChannels { get; set; }
 }
