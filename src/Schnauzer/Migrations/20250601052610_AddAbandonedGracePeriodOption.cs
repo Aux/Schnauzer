43c759875137675485af61934431e6eb15820a4f
Aux <817531+Aux@users.noreply.github.com>
2025-06-01 00:49:34 -0500

    Add migration for AbandonedGracePeriod option

---
+++ src/Schnauzer/Migrations/20250601052610_AddAbandonedGracePeriodOption.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schnauzer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAbandonedGracePeriodOption : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "AbandonedGracePeriod",
+                table: "Guilds",
+                type: "interval",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbandonedGracePeriod",
+                table: "Guilds");
+        }
+    }
+}
